7901833ab862ed8a4c100f23c33f47088621f3d3
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-21 12:27:31 +0900

    finished creating jumbotron & css

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Container, Inner, Item, Title, SubTitle } from "./styles/Jumbotron";
+import {
+  Container,
+  Item,
+  Inner,
+  Title,
+  SubTitle,
+  Image,
+  Pane,
+} from "./styles/Jumbotron";
 
-const Jumbotron = ({ children, direction = "row", ...restProps }) => {
-  <Item>
-    <Inner direction={direction}>{children}</Inner>
-  </Item>;
-};
-
-export default Jumbotron;
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+}
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
@@ -19,4 +31,12 @@ Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
 
 Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
   return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
 };
