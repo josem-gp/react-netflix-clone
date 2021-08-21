faef4a98840432fec7d9b59dc5fdda73b66ba613
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-21 10:28:55 +0900

    created jumbotron component

--- src/components/jumbotron/styles/Jumbotron.js
+++ src/components/jumbotron/styles/Jumbotron.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+const Jumbotron = ({ children, direction = "row", ...restProps }) => {
+  <Item>
+    <Inner direction={direction}>{children}</Inner>
+  </Item>;
+};
+
+export default Jumbotron;
