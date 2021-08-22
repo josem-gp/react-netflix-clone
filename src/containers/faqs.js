35d4ffd66d4ad9d9eb9046dd588d3bbbc63c8c61
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-23 06:49:32 +0900

    Updated the rendering of Accordion with the new Compound Component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -5,13 +5,14 @@ import faqsData from "../fixtures/faqs.json";
 export function FaqsContainer() {
   return (
     <Accordion>
-      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-      {faqsData.map((item) => (
-        <Accordion.Item key={item.id}>
-          <Accordion.Header>{item.header}</Accordion.Header>
-          <Accordion.Body>{item.body}</Accordion.Body>
-        </Accordion.Item>
-      ))}
+      <Accordion.Frame>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion.Frame>
     </Accordion>
   );
 }
