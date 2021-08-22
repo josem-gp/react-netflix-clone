52e71c2f640e079bb7000f4e2a7f9f5fc63cb5c0
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-22 19:38:40 +0900

    fixed imports bugs and rendered footer in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { JumbotronContainer } from "../containers/Jumbotron";
+import { FooterContainer } from "../containers/Footer";
 
 const Home = () => {
   return (
     <div>
       <JumbotronContainer />
+      <FooterContainer />
     </div>
   );
 };
