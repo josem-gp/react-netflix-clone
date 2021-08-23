d3920746f99acf1faf8a07baad85f6a583b04ec9
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-23 19:04:49 +0900

    added new style to form

--- src/components/opt-form/styles/Opt-form.js
+++ src/components/opt-form/styles/Opt-form.js
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  margin-top: 20px;
+  flex-wrap: wrap;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
 export const Input = styled.input`
   max-width: 450px;
   width: 100%;
