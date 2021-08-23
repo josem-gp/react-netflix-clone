cdac0cc74a0b2e3c0e3e48a285d80f3651aab84d
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-23 18:54:10 +0900

    rendered Opt-form in the Faqs Container

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Accordion } from "../components";
+import { Accordion, OptForm } from "../components";
 import faqsData from "../fixtures/faqs.json";
 
 export function FaqsContainer() {
@@ -13,6 +13,9 @@ export function FaqsContainer() {
           </Accordion.Item>
         ))}
       </Accordion.Frame>
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+      </OptForm>
     </Accordion>
   );
 }
