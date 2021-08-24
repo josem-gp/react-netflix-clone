eb313e44e7b4092af47c519e3ad685915a4d80f1
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-24 18:07:24 +0900

    rendered header with form

--- src/components/header/styles/Header.js
+++ src/components/header/styles/Header.js
@@ -4,8 +4,11 @@ import { Link as ReachRouterLink } from "react-router-dom";
 export const Background = styled.section`
   display: flex;
   flex-direction: column;
-  background: url(${({ src }) => (src ? `${src}.jpg` : "home-bg.jpg")}) top left /
-    cover no-repeat;
+  background: url(${({ src }) =>
+      src
+        ? `/public/images/misc/${src}.jpg`
+        : "/public/images/misc/home-bg.jpg"})
+    top left / cover no-repeat;
 
   @media (max-width: 1100px) {
     ${({ dontShowOnSmallViewPort }) =>
