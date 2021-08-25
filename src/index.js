77bbed2d88c0a1a93baad2c4b2bba1fe49ff4c67
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-25 11:10:17 +0900

    initialized app and added the values for config firebase

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import App from "./App";
 import { FirebaseContext } from "./context/firebase";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyAxXkyUlpz013wv2Rv0JUB1WnJ5G-aYRqo",
+  authDomain: "netflix-clone-a94a9.firebaseapp.com",
+  projectId: "netflix-clone-a94a9",
+  storageBucket: "netflix-clone-a94a9.appspot.com",
+  messagingSenderId: "71145020139",
+  appId: "1:71145020139:web:dfdf3d61034653e9e3bcae",
 };
 
 const firebase = window.firebase.initializeApp(config);
