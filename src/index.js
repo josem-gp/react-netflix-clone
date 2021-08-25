d6e094f5b26be413271b168c15a1209381cb624a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-25 09:35:23 +0900

    started configuration on Firebase

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import App from "./App";
+import { FirebaseContext } from "./context/firebase";
 
-import reportWebVitals from "./reportWebVitals";
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <>
+  <>
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
       <GlobalStyles />
       <App />
-    </>
-  </React.StrictMode>,
+    </FirebaseContext.Provider>
+  </>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
