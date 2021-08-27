631ac4a72f8d895efe882a4dbcbf145e4c1588cd
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-27 22:52:24 +0900

    added Header to display of browser

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -15,6 +15,29 @@ export function BrowseContainer() {
 
   return profile.displayName ? (
     <>
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo
+              to={ROUTES.HOME}
+              src="/images/misc/logo.svg"
+              alt="Netflix"
+            />
+            <Header.TextLink
+              active={category === "series" ? "true" : "false"}
+              onClick={() => setCategory("series")}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={category === "films" ? "true" : "false"}
+              onClick={() => setCategory("films")}
+            >
+              Films
+            </Header.TextLink>
+          </Header.Group>
+        </Header.Frame>
+      </Header>
       <FooterContainer />
     </>
   ) : (
