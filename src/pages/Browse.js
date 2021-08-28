16aee4287240a5e447c3d558833d132e29982996
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-28 12:10:30 +0900

    fixed bug import

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowseContainer } from "../containers/browse";
 import { useContent } from "../hooks";
+import { selectionMap } from "../utils";
 
 export default function Browse() {
   const { series } = useContent("series");
