39dcaeaaebe6846f7017c6c8ed664c80e6c762fb
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-28 11:25:24 +0900

    added dropdown functionality to browser

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -14,6 +14,9 @@ import {
   Search,
   SearchIcon,
   SearchInput,
+  Profile,
+  Picture,
+  Dropdown,
 } from "./styles/Header";
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -83,4 +86,16 @@ Header.FeatureCallOut = function HeaderFeatureCallOut({
 
 Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
   return <PlayButton {...restProps}>{children}</PlayButton>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
 };
